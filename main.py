ed432cf1e0545cd51ca9185121d87067eef04f11
Saqib Rashid <saqibrashid446@gmail.com>
2026-07-29 15:43:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
-from google import genai
 import streamlit as st
-import os 
+import os
+from google import genai
 from dotenv import load_dotenv
-load_dotenv() 
-GEMINI_API_KEY=os.getenv("GEMINI_API_KEY")
-title = st.title("welcome to my chatbot")
-user_input = st.chat_input("write  something here")
+
+load_dotenv()
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+st.title(" Welcome to my Chatbot")
+
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.write(message["text"])
+
+
+user_input = st.chat_input("Write something here...")
 
 if user_input:
+    
+    with st.chat_message("user"):
+        st.write(user_input)
+    
+    
+    st.session_state.chat_history.append({"role": "user", "text": user_input})
+    
+    
     client = genai.Client(api_key=GEMINI_API_KEY)
-    interactions = client.interactions.create(
-     model="gemini-3.6-flash",
-    input=user_input,
-)
-    st.write(interactions.output_text)
-
-
-
-
-   
-
-
+    
+    with st.spinner("AI is thinking..."):
+        interactions = client.interactions.create(
+                model="gemini-3.6-flash",
+                input=user_input, 
+            )
+    bot_response = interactions.output_text
+     
+     
+    with st.chat_message("assistant"):
+         st.write(bot_response)
+         
+         st.session_state.chat_history.append({"role": "assistant", "text": bot_response})
