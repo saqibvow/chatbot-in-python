7f1eb7c24dbff53eb97b6025b6ac62c9941b1819
Saqib Rashid <saqibrashid446@gmail.com>
2026-07-31 17:29:54 +0500

    using pollination ai tm I will understand what is session state in streamlit

--- main.py
+++ main.py
@@ -1,44 +1,16 @@
 import streamlit as st
-import os
-from google import genai
-from dotenv import load_dotenv
+from openai import OpenAI
 
-load_dotenv()
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-
-st.title(" Welcome to my Chatbot")
-
+user_input = st.chat_input("write something if you want a response from Ai")
 
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
 
-for message in st.session_state.chat_history:
-    with st.chat_message(message["role"]):
-        st.write(message["text"])
-
-
-user_input = st.chat_input("Write something here...")
-
+for chat in st.session_state.chat_history:
+    st.write(chat["content"])
 if user_input:
-    
-    with st.chat_message("user"):
-        st.write(user_input)
-    
-    
-    st.session_state.chat_history.append({"role": "user", "text": user_input})
-    
-    
-    client = genai.Client(api_key=GEMINI_API_KEY)
-    
-    with st.spinner("AI is thinking..."):
-        interactions = client.interactions.create(
-                model="gemini-3.6-flash",
-                input=user_input, 
-            )
-    bot_response = interactions.output_text
-     
-     
-    with st.chat_message("assistant"):
-         st.write(bot_response)
-         
-         st.session_state.chat_history.append({"role": "assistant", "text": bot_response})
+    client = OpenAI(base_url="https://gen.pollinations.ai/v1", api_key="sk_UtdfC8O4gpwGAWDGD9Af2btUJzZanlPV")
+    response = client.chat.completions.create(model="openai", messages=[{"role": "user", "content": user_input}])
+    output = st.text(response.choices[0].message.content)
+    st.session_state.chat_history.append(user_input)
+    print(response.choices[0].message.content,flush=True)
