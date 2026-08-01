cbae98b080e05637ce993323341b0207fe7ddb4b
Saqib Rashid <saqibrashid446@gmail.com>
2026-08-01 16:10:49 +0500

    better ui  and hide  secrets files and  now  i will continue my this project tm

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
 import streamlit as st
 from openai import OpenAI
 
-user_input = st.chat_input("write something if you want a response from Ai")
+# ------------------client instance initialize ------------------
 
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = []
+client = OpenAI(base_url="https://gen.pollinations.ai/v1", api_key=st.secrets[("POLLINATIONS_API_KEY ")]
 
-for chat in st.session_state.chat_history:
-    st.write(chat["content"])
-if user_input:
-    client = OpenAI(base_url="https://gen.pollinations.ai/v1", api_key="sk_UtdfC8O4gpwGAWDGD9Af2btUJzZanlPV")
-    response = client.chat.completions.create(model="openai", messages=[{"role": "user", "content": user_input}])
-    output = st.text(response.choices[0].message.content)
-    st.session_state.chat_history.append(user_input)
-    print(response.choices[0].message.content,flush=True)
+
+# ------ session state for saving data of simple  variables ------------
+
+if "messages" not in st.session_state:
+   st.session_state.messages = []
+
+for chat in  st.session_state.messages:
+    st.chat_message(chat["role"])
+    st.markdown(chat["content"])
+
+# -------------side-bar rerun  option-----------------
+
+
+with  st.sidebar:
+    st.header("Rerun option")
+    st.title("!Attention")
+    st.caption("If you click Rerun button the chat will be reset ")
+    if st.button("Rerun"):
+        st.session_state.messages= []
+
+        
+
+
+# ------------ user input -------------------
+
+
+
+user_input = st.chat_input(
+    "write something if you want a response from Ai",
+    submit_mode= "disable"
+
+ )
+
+if user_input and user_input.strip():
+    st.session_state.messages.append({"role":"user","content":user_input})
+    st.chat_message("user").write(user_input)
+
+
+    # ------------------------ LLM Response -------------------------- 
+
+
+    with st.spinner("🤖 Thinking..."):
+        response = client.chat.completions.create(model="openai", messages=st.session_state.messages)
+        ai_response= response.choices[0].message.content
+     
+    st.chat_message("assistant").write(ai_response)
+    st.session_state.messages.append({"role":"assistant","content":ai_response})
